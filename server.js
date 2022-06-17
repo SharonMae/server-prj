baf68bb97ddd4fe033c21aaa97710249144dd56f
Sharon Mae <duransharonmae@gmail.com>
2022-06-17 21:21:46 +0200

    feat: add server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.port || 5000;
+
+app.use(express.static('public'));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/test.html')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
